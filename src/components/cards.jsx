9a094bbf265c32c602d44cbc7c3a93dbbef26214
carlos del saz <delsaz9999@gmail.com>
2024-03-22 08:27:51 +0100

    Espero que el ciommit final del 22/03/24

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,5 +1,12 @@
 import "../styles/Cards.css";
 import imgCard from "../../public/images/desktop/image-deep-earth.jpg"
+import imgCard2 from "../../public/images/desktop/image-night-arcade.jpg"
+import imgCard3 from "../../public/images/desktop/image-soccer-team.jpg"
+import imgCard4 from "../../public/images/desktop/image-grid.jpg"
+import imgCard5 from "../../public/images/desktop/image-from-above.jpg"
+import imgCard6 from "../../public/images/desktop/image-pocket-borealis.jpg"
+import imgCard7 from "../../public/images/desktop/image-curiosity.jpg"
+import imgCard8 from "../../public/images/desktop/image-fisheye.jpg"
 //import {card} from "../components/data/data";
 
 
@@ -18,6 +25,41 @@ const Cards = () => {
       <img src={imgCard}/>
       <h2>Deep</h2>
       <h2>Earth</h2>
+    </div>
+    <div className="cards">
+      <img src={imgCard2}/>
+      <h2>Night</h2>
+      <h2>Arcade</h2>
+    </div>
+    <div className="cards">
+      <img src={imgCard3}/>
+      <h2>Soccer</h2>
+      <h2>Team vr</h2>
+    </div>
+    <div className="cards">
+      <img src={imgCard4}/>
+      <h2>The</h2>
+      <h2>Grid</h2>
+    </div>
+    <div className="cards">
+      <img src={imgCard5}/>
+      <h2>From</h2>
+      <h2>Above</h2>
+    </div>
+    <div className="cards">
+      <img src={imgCard6}/>
+      <h2>Pocket</h2>
+      <h2>Borealis</h2>
+    </div>
+    <div className="cards">
+      <img src={imgCard7}/>
+      <h2>The</h2>
+      <h2>Curiosity</h2>
+    </div>
+    <div className="cards">
+      <img src={imgCard8}/>
+      <h2>The</h2>
+      <h2>Fisheye</h2>
     </div>
     </section>
   )
