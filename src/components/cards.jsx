1aaf1c0a4605e743d50adc6fdbd0edb3427924af
carlos del saz <delsaz9999@gmail.com>
2024-03-22 08:00:41 +0100

    No se cual commit ya 22/03/24

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,18 +1,24 @@
 import "../styles/Cards.css";
-import {card} from "../components/data/data";
+import imgCard from "../../public/images/desktop/image-deep-earth.jpg"
+//import {card} from "../components/data/data";
 
 
 const Cards = () => {
   return(
     <section className="container__cards">
-    {card.map((item, index) => (
+    {/*{card.map((item, index) => (
     <div className="cards" key={index}>
       <img src={item.imgCard}/>
       <h2>{item.titulo}</h2>
       <h2>{item.titulo2}</h2>
     </div>
 
-    ))}
+    ))}*/}
+    <div className="cards">
+      <img src={imgCard}/>
+      <h2>Deep</h2>
+      <h2>Earth</h2>
+    </div>
     </section>
   )
 } 
